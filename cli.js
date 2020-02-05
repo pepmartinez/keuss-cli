f51f7853ffc138dfa80641a31e7be273c21e4759
pepe <pep.martinez@gmail.com>
2020-02-05 19:39:42 +0100

    use keuss 1.5.6, adds support for pause/resume

--- cli.js
+++ cli.js
@@ -17,6 +17,8 @@ program
   .usage   ('[options]')
   .option  ('-q, --queue [queue]', 'act on this queue')
   .option  ('-i, --info', 'get info about queue')
+  .option  ('-0, --pause', 'pause queue')
+  .option  ('-1, --resume', 'resume queue')
   .option  ('-c, --consumer <n>', 'run consumer loop with n consumers', parseInt)
   .option  ('-C, --consumer-num <n>', 'consume n elements', parseInt)
   .option  ('-p, --producer <n>', 'run producer loop with n producers', parseInt)
@@ -35,16 +37,17 @@ program
 /////////////////////////////////////////
 function info (q, cb) {
   async.parallel ({
-    size:         (cb) => {q.size (cb)},
-    totalSize:    (cb) => {q.totalSize (cb)},
-    schedSize:    (cb) => {q.schedSize (cb)},
-    next_t:       (cb) => {q.next_t (cb)},
-    stats:        (cb) => {q.stats (cb)},
-    topology:     (cb) => {q.topology (cb)},
-    name:         (cb) => {cb (null, q.name())},
-    ns:           (cb) => {cb (null, q.ns())},
-    type:         (cb) => {cb (null, q.type())},
-    capabilities: (cb) => {cb (null, q.capabilities())},
+    size:         cb => q.size (cb),
+    totalSize:    cb => q.totalSize (cb),
+    schedSize:    cb => q.schedSize (cb),
+    next_t:       cb => q.next_t (cb),
+    stats:        cb => q.stats (cb),
+    paused:       cb => q.paused (cb),
+    topology:     cb => q.topology (cb),
+    name:         cb => cb (null, q.name()),
+    ns:           cb => cb (null, q.ns()),
+    type:         cb => cb (null, q.type()),
+    capabilities: cb => cb (null, q.capabilities()),
   }, cb);
 }
 
@@ -52,24 +55,22 @@ function info (q, cb) {
 /////////////////////////////////////////
 function consume_loop (q, n, cb) {
   if (n == 0) return cb ();
-  
-  q.pop ('keuss-cli', function (err, res) {
-    if (err) return cb (err);    
+
+  q.pop ('keuss-cli', (err, res) => {
+    if (err) return cb (err);
 
     if (program.dumpProduced) {
       console.log ('%j', res, {});
     }
-    
+
     if (program.verbose) {
       console.log ('consume_loop: got element');
     }
-    
+
     var next_n = _.isNil (n) ? n : (n ? n - 1 : n);
 
     if (program.consumerLoopDelay) {
-      setTimeout (function () {
-        consume_loop (q, next_n, cb);
-      }, program.consumerLoopDelay)
+      setTimeout (() => consume_loop (q, next_n, cb), program.consumerLoopDelay);
     }
     else {
       consume_loop (q, next_n, cb);
@@ -79,35 +80,33 @@ function consume_loop (q, n, cb) {
 
 
 /////////////////////////////////////////
-function produce_loop (q, n, cb) {   
+function produce_loop (q, n, cb) {
   if (n == 0) return cb ();
-  
+
   var opts = {};
   if (program.producerDelay) {
     opts.delay = program.producerDelay;
   }
-  
+
   var obj = objs[chance.integer ({min:0, max:110})];
-  q.push (obj, opts, function (err, res) {
+  q.push (obj, opts, (err, res) => {
     if (err) {
       if (err == 'drain') {
         console.log ('queue in drain, stopping producer');
         return;
       }
-      
+
       return cb (err);
     }
 
     if (program.verbose) {
-      console.log ('produce_loop: put %j', obj); 
+      console.log ('produce_loop: put %j', obj);
     }
-  
+
     var next_n = _.isNil (n) ? n : (n ? n - 1 : n);
-    
+
     if (program.producerLoopDelay) {
-      setTimeout (function () {
-        produce_loop (q, next_n, cb);
-      }, program.producerLoopDelay)
+      setTimeout (() => produce_loop (q, next_n, cb), program.producerLoopDelay);
     }
     else {
       produce_loop (q, next_n, cb);
@@ -118,70 +117,76 @@ function produce_loop (q, n, cb) {
 
 
 var q_opts = {};
-  
+
 console.log (`MQ.init: using backend ${program.backend || 'mongo'}`);
 
 if (program.signaller) {
   var signal_provider = require ('keuss/signal/' + program.signaller);
   q_opts.signaller = {
     provider: signal_provider
-  }
+  };
 
   console.log (`MQ.init: using signeller ${program.signaller}`);
 }
-  
+
 if (program.stats) {
   var stats_provider = require ('keuss/stats/' + program.stats);
   q_opts.stats = {
     provider: stats_provider
-  }
+  };
 
   console.log (`MQ.init: using stats ${program.stats}`);
 }
 
 
 var MQ = require ('keuss/backends/' + (program.backend || 'mongo'));
-MQ (q_opts, function (err, factory) {
+MQ (q_opts, (err, factory) => {
   if (err) return console.error ('MQ.init: %s', err, {});
-  
+
   console.log ('MQ.init: backend initiated');
-  
+
   var q = factory.queue (program.queue || 'test', {});
 
   var tasks = [];
-  
-  if (program.info) {
-    tasks.push (function (cb) {
-      info (q, function (err, res) {
-        if (err) {
-          console.error (err);
-        }
-        else {
-          console.log (res);
-        }
 
-        cb ();
-      });
-    })
+  if (program.info) {
+    tasks.push (cb => info (q, (err, res) => {
+      if (err) {
+        console.error (err);
+      }
+      else {
+        console.log (res);
+      }
+
+      cb ();
+    }));
+  }
+
+  if (program.pause) {
+    tasks.push (cb => {q.pause (true); cb(); });
+  }
+
+  if (program.resume) {
+    tasks.push (cb => {q.pause (false); cb(); });
   }
 
   for (var c = 0; c < program.consumer; c++) {
-    tasks.push (function (cb) {
+    tasks.push (cb => {
       console.log ('MQ.init: initiating consume loop');
       consume_loop (q, program.consumerNum, cb);
     });
   }
-  
+
   for (p = 0; p < program.producer; p++) {
-    tasks.push (function (cb) {
+    tasks.push (cb => {
       console.log ('MQ.init: initiating produce loop with ', program.producerNum);
       produce_loop (q, program.producerNum, cb);
     });
   }
 
 //  tasks.push ((cb) => setTimeout (cb, 2000));
-  
-  async.parallel (tasks, function (err) {
+
+  async.parallel (tasks, err => {
     if (err) {
       console.error (err);
     }
@@ -194,10 +199,10 @@ MQ (q_opts, function (err, factory) {
   function _farewell_and_good_night () {
     console.log ('farewell...');
     async.series ([
-      (cb) => q.drain (cb),
-      (cb) => {factory.close(); cb ();},
-      (cb) => {q.cancel (); cb ();}
-    ], function () {
+      cb => q.drain (cb),
+      cb => {factory.close(); cb ();},
+      cb => {q.cancel (); cb ();}
+    ], () => {
       if (err) console.error (err);
       console.log ('...and good night');
     });
